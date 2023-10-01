a57ebe59c79337f2f257b73414671d9b4ff549fd
Vedanth Ramji <86137377+Vedanth-Ramji@users.noreply.github.com>
2023-10-01 14:30:50 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000/submissions";
+const url = "https://thaavaram.onrender.com/submissions";
 
 export const fetchSubmissions = () => axios.get(url);
 export const createSubmission = (submission) => axios.post(url, submission);
