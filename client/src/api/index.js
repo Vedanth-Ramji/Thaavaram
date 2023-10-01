1be99cd894b785cbfb4a5e17d107411ca528bc55
Vedanth Ramji <86137377+Vedanth-Ramji@users.noreply.github.com>
2023-10-01 14:01:28 +0530

    Delete client directory

--- client/src/api/index.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-const url = "http://localhost:5000/submissions";
-
-export const fetchSubmissions = () => axios.get(url);
-export const createSubmission = (submission) => axios.post(url, submission);
