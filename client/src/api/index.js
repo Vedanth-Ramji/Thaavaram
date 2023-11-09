15e70eef15a79fddbc4ab530074db8ae05128d1f
Vedanth Ramji <86137377+Vedanth-Ramji@users.noreply.github.com>
2023-11-09 15:03:06 +0530

    Delete client/src directory

--- client/src/api/index.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-const url = "https://thaavaram.onrender.com/submissions";
-
-export const fetchSubmissions = () => axios.get(url);
-export const createSubmission = (submission) => axios.post(url, submission);
