4886740c70664c64a976e377eb444ec3ec829eed
Vedanth Ramji <86137377+Vedanth-Ramji@users.noreply.github.com>
2023-11-10 12:28:24 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import "./globalStyles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Header from "./components/header";
+
+import HomePage from "./pages/homePage";
+import SubmitPage from "./pages/submitPage";
+import BrowsePage from "./pages/browsePage";
+import RegisterPage from "./pages/registerPage";
+
+import { getSubmissions } from "./actions/submissionsActions";
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getSubmissions());
+  }, [dispatch]);
+
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/submit" element={<SubmitPage />} />
+        <Route path="/browse" element={<BrowsePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
