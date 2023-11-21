9d18f745cd35e555775f2edaaa572592baecd740
Vedanth Ramji <86137377+Vedanth-Ramji@users.noreply.github.com>
2023-11-21 10:53:26 +0530

    Update announcementBanner.js

--- client/src/components/announcementBanner.js
+++ client/src/components/announcementBanner.js
@@ -4,7 +4,7 @@ const AnnouncementBanner = () => {
   return (
     <div className="announcementBanner">
       <h6>
-        World Congress: AMR in Plants <a href="https://forms.gle/HZSMQjScvGx7TwvA7" target="_blank"> Register Now!</a>
+        World Congress: AMR in Plants <div><a href="https://forms.gle/HZSMQjScvGx7TwvA7" target="_blank"> Register Now!</a></div> <div><Link style={{marginLeft: 15}} to='/world-congress'>Learn More</Link></div>
       </h6>
     </div>
   );
