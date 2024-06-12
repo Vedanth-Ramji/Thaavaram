0409e305f4b7d71596957e79e919b2802cbe1679
Vedanth <vedanth.ramji@outlook.com>
2024-06-12 10:31:16 +0530

    Added drug classes pie chart

--- client/src/pages/browsePage.js
+++ client/src/pages/browsePage.js
@@ -77,6 +77,10 @@ const BrowsePage = () => {
     <h1>Taxon heatmaps of known pathogens from global metagenomics data</h1>
     <img width={'100%'} src={require('../assets/taxon_heatmap.png')}></img>
     </div>
+    <div style={{textAlign: 'center', padding: 20}}>
+    <h1>AMR drug classes found from global metagenomics data</h1>
+    <img width={'100%'} src={require('../assets/drug_classes.png')}></img>
+    </div>
     <div style={{textAlign: 'center'}}>
       <h1 style={{padding: 20, textAlign: 'center'}}>Data Processing at Thaavaram</h1>
       <img style={{margin: 20}} width={'60%'} src={require('../assets/data_processing.png')}></img>
